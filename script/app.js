6e8040557462f8fbb97d7cd19ba257d813492ce3
MuhammadMuzammil-TheDeveloper <muzammil.thedeveloper@gmail.com>
2026-04-25 21:04:18 +0500

    Start

---
+++ script/app.js
@@ -0,0 +1,10 @@
+const loginUserEmail = document.getElementById("loginUserEmail");
+const loginUserPassword = document.getElementById("loginUserPassword");
+const loginBtn = document.getElementById("loginBtn")
+
+loginBtn.addEventListener('click', ()=>{
+    console.log("working ....")
+    console.log("email",loginUserEmail.value)
+    console.log("passoword",loginUserPassword.value)
+    if(login)
+})
